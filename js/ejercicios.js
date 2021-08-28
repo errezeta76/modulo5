34aac1ca006d4caf2839219f4c0060b676524463
errezeta76 <rzc.info@gmail.com>
2021-08-27 22:57:45 -0400

    java

---
+++ js/ejercicios.js
@@ -0,0 +1,86 @@
+// ejercicio 1
+let n = 10
+let m = 40
+let r= n + m
+console.log( r )
+
+// ejercicio 2
+
+let x = 2
+let z = 3
+let y = 4
+let n4 = 6
+let n5 = 6
+let mul= x * z * y * n4 * n5
+
+console.log( mul )
+
+//ejercicio  3
+let p =((x + z) * y * y)
+console.log ( p )
+
+//ejerciio 4
+
+let a = "hola"
+let b = " mundo"
+console.log( a + b )
+
+//ejercicio 5
+let na = "5"
+let nb = "3"
+let nc = "7"
+let xx = "5 + 3 + 7 ="
+console.log (xx + na + nb + nc )
+
+
+
+let ww = "Vamonos a "
+let bb = " Breack"
+console.log( ww + bb )
+
+//ejercicios 6
+
+let usuario1 = { nombre: "juan", apellido: "perez" }
+let usuario2 = { nombre: "sara", apellido: "aparicio" }
+let usuario3 = { nombre: "sebastián", apellido: "gonzales" }
+let usuario4 = { nombre: "catalina", apellido: "rodriguez" }
+let usuario5 = { nombre: "laura", apellido: "quintanilla" }
+let usuario6 = { nombre: "camila", apellido: "lopez" }
+let usuario7 = { nombre: "carlos", apellido: "gutierrez" }
+let usuario8 = { nombre: "sergio", apellido: "chacon" }
+x = 5
+if (x == 1) {
+    console.log( "hola " + usuario1.nombre + "  " + usuario1.apellido) }    
+ else { if (x == 2) {
+    console.log( "hola " + usuario2.nombre + "  " + usuario2.apellido)  
+}else{ if (x == 3) {
+    console.log( "hola " + usuario3.nombre + "  " + usuario3.apellido)  
+}else{ if (x == 4) {
+    console.log( "hola " + usuario4.nombre + "  " + usuario4.apellido)  
+}else { if (x == 5) {
+    console.log( "hola " + usuario5.nombre + "  " + usuario5.apellido)  
+}else { if (x == 6) {
+    console.log( "hola " + usuario6.nombre + "  " + usuario6.apellido)  
+}else { if (x == 7) {
+    console.log( "hola " + usuario7.nombre + "  " + usuario7.apellido)  
+}else { if (x == 8) {
+    console.log( "hola " + usuario8.nombre + "  " + usuario8.apellido)  
+}}}}}}}}
+
+//arreglo
+
+let usuarios = [usuario1, usuario2, usuario3, usuario4, usuario5, usuario6, usuario7, usuario8]
+
+if   (n   <=   usuarios . length )  {      ... de René Rodriguez Clavijo
+    René Rodriguez Clavijo22:59
+    if (n <= usuarios.length) {    
+        console.log("Hola con arreglo, " + usuarios[n-1].nombre + " " + usuarios[n-1].apellido)
+    }else{    
+        console.log("Usuario no registrado")    
+    }
+
+
+
+
+
+
